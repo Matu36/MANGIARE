970862c507ba81f48411970ad731e174b5597899
YamilLeotta <yamil.leotta@live.com>
2023-02-12 17:36:42 -0300

    add POST User routes & controller

---
+++ API/src/controllers/postUser.js
@@ -0,0 +1,18 @@
+const {Users} = require('../db.js');
+
+module.exports = async (req, res) => {
+  try{
+    if ((!req.body?.username) || ((!req.body?.email))) throw 'No body params'
+
+    const [instance, created] = await Users.findOrCreate({
+      where: {email: req.body.email.toLowerCase()},
+      defaults: {...req.body, email: req.body.email.toLowerCase()}
+  });
+
+    res.send(instance);
+  }
+  catch(error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
