9c129c2f52183b4ceea455cb36cfbc6c8587af44
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-13 13:00:44 -0300

    localStorage deleted when user logout

--- FRONT/src/components/Auth0/logout_button.js
+++ FRONT/src/components/Auth0/logout_button.js
@@ -4,17 +4,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 export function LogoutButton() {
   const { logout } = useAuth0();
 
-  return (
-    <button
-      onClick={() => {
-        logout({
-          logoutParams: {
-            returnTo: window.location.origin,
-          },
-        });
-      }}
-    >
-      Log out
-    </button>
-  );
+  const loggingOut = () => {
+    localStorage.removeItem("MANGIARE_user");
+    localStorage.removeItem("MANGIARE_cart");
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  return <button onClick={loggingOut}>Log out</button>;
 }
