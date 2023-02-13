fbce8280ded69fa5543f3a9d09462c536faadf13
YamilLeotta <yamil.leotta@live.com>
2023-02-13 03:04:38 -0300

    add some keys in GET /recipes

--- API/src/controllers/getRecipes.js
+++ API/src/controllers/getRecipes.js
@@ -1,16 +1,16 @@
-const {Recipes} = require('../db.js');
+const {Recipes, Recipe_ingredients, Ingredients} = require('../db.js');
 
 module.exports = async (req, res) => {
   try{
     let recipes = await Recipes.findAll(
       (!req.params?.id)
-        ? {include: ['Recipe_diets', 'Recipe_ingredients']}
-        : {include: ['Recipe_diets', 'Recipe_ingredients'], where: {id: req.params.id}}
+        ? {include: ['Recipe_diets', {model: Recipe_ingredients, include: {model: Ingredients}}, 'Reviews']}
+        : {include: ['Recipe_diets', {model: Recipe_ingredients, include: {model: Ingredients}}, 'Reviews'], where: {id: req.params.id}}
     )
 
     return (!recipes)
       ? res.status(404).send('Recipes Not Found')
-      : res.send(recipes.map(({id, title, instructions, image, Recipe_diets, Recipe_ingredients}) => ({id, title, instructions, image, diets: Recipe_diets.map(({diet}) => diet), ingredients: Recipe_ingredients.map(({ingredientId, amount, unit}) => ({id: ingredientId, amount, unit}))})));
+      : res.send(recipes.map(({id, title, instructions, Reviews, image, Recipe_diets, Recipe_ingredients, userId}) => ({id, title, instructions, image, userId, diets: Recipe_diets.map(({diet}) => diet), Reviews, rating: Math.round(Reviews.reduce((aux, {rate}) => aux + rate, 0) / Reviews.length), ingredients: Recipe_ingredients.map(({ingredientId, amount, unit}) => ({id: ingredientId, amount, unit})), price: Recipe_ingredients.reduce((aux, {Ingredient, amount}) => aux + Ingredient.price * amount, 0)})));
   }
   catch(error) {
     console.log(error);
