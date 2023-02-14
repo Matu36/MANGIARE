f951d677f24644cec85c57b5a94d2f69be514206
Your Name~ <you@example.com>
2023-02-14 09:19:18 -0300

    Home page restyling

--- FRONT/src/pages/ShoppingCart/ShoppingCart.jsx
+++ FRONT/src/pages/ShoppingCart/ShoppingCart.jsx
@@ -51,35 +51,68 @@ export default function ShoppingCart () {
             })
     }
 
-    return (
-        <div style={{width: '50%', margin: 'auto'}}>
-            {
-                (!cart?.length)
-                    ? <h2>The Shopping Cart is empty...</h2>
-                    : (<>
-                        <IngredientsList
-                            items = {cart.map(el => ({...el, units: [el.unit]}))}
-                            onChange = {handleOnChange}
-                            itemButton = {{
-                                caption: 'Remove Item',
-                                action: handleOnDelete
-                            }}
-                        />
-                        <br />
-                        <p>Total: ${cart.reduce((aux, el) => aux + el.amount * el.price, 0).toFixed(2)}</p>
-                        <center>
-                        {
-                            email
-                                ? <button onClick={handleCheckout}>Checkout</button>
-                                : <><p>You must login before proceed to checkout</p> <LoginButton /></>
-                        }
-                        </center>
-                    </>)
-            }
-            {
-                JSON.parse(localStorage.getItem('pendingPayment')) && (<><p>Order #{JSON.parse(localStorage.getItem('pendingPayment')).order.orderId} has been created!</p> <button onClick={handlePay}>PAY</button></>)
-            }
-        </div>
-    )
+   
+        return (
+            < Box
+         width="100%"
+         height="100vh"
+         marginTop="1px"        
+         style={{
+           display: "flex",
+           alignItems: "left",
+           justifyContent: "left",
+           flexDirection: "row",
+           backgroundSize: "cover",
+           backgroundPosition: "center center",
+         }}
+       >
+         <Flex>
+             <Box w='40%' h='100vh' bgImage={background} style={{          
+           backgroundSize: "cover",
+           }} />
+             <Spacer width='400px'/>
+             <Box w = '600px'>
+                 <Text fontSize="6xl" fontWeight="bold" color="yellow.500" style={{textAlign: "center"}}>
+                   Shopping Cart
+                 </Text>
+                 <VStack spacing={10} align="stretch">
+                
+                 {
+                     !this.props.cart.length
+                         ? <h2>The Shopping Cart is empty...</h2>
+                         : (<>
+                             <IngredientsList
+                                 items = {this.props.cart.map(el => ({...el, units: [el.unit]}))}
+                                 onChange = {this.handleOnChange}
+                                 itemButton = {{
+                                     caption: 'Remove Item',
+                                     action: this.handleOnDelete
+                                 }}
+                             />
+                             <br />
+                             <Text fontSize="3xl" fontWeight="bold" color="green.500" style={{textAlign: "left"}}>
+                             >Total: ${this.props.cart.reduce((aux, el) => aux + el.amount * el.price, 0).toFixed(2)}</Text>
+                             <center>
+                                 
+                                 <Button  onClick={() => this.handleCheckout(this.props.cart)}  colorScheme="teal"  variant="solid" size="lg"
+ >
+   Checkout
+ </Button></center>
+                         </>)
+                 }
+                 {
+                     this.state.order?.orderId && (<><p>Order #{this.state.order.orderId} has been created!</p> <button onClick={this.handlePay}>PAY</button></>)
+                 }
+ 
+                 </VStack>
+                 </Box>
+                 
+ 
+                 <Box w = '400px'/>
+                
+             </Flex>
+         </Box>
+         )
+     }
     
-}
+ 
