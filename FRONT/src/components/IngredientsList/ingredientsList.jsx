f7ba94d327d35684d3aa9ed11c126b8cc86dc80f
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-14 16:39:39 -0300

    fixed add button

--- FRONT/src/components/IngredientsList/ingredientsList.jsx
+++ FRONT/src/components/IngredientsList/ingredientsList.jsx
@@ -41,7 +41,7 @@ export default function IngredientsList (props) {
             </td>
             <td>
               <Button name="itemButton" onClick={() => props.itemButton.action(el.id, el.unit)}>
-                Add Item
+              {props.itemButton.caption}
               </Button>
             </td>
             <td>
