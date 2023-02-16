d7c3db90ca477927bbc36bc467416a56c44ae336
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-16 18:29:55 -0300

    add link to vercel deploy in /mailer/template/newUser

--- API/src/mailer/templates/newUser.js
+++ API/src/mailer/templates/newUser.js
@@ -40,7 +40,7 @@ const newUser = (props) => {
     <p>Have you ever had the feeling that you have a bunch of ingredients in your pantry and don't know how to combine them to create something tasty? Mangiare is here to solve that problem. Simply enter the ingredients you have at home, and our app will show you a list of delicious recipes you can make with them. It's that easy!</p>
     <p>In addition, we make sure that our recipes are simple and easy to follow, even if you're a beginner cook. And if you're looking for something specific, you can use our advanced search tools to find the perfect recipe.</p>
     <p>We want you to enjoy cooking, so we've made Mangiare as easy as possible. Plus, we're constantly working to improve our app and add new features. And the best part is, Mangiare is completely free.</p>
-    <p>Are you ready to get started? Log in now and start cooking with your ingredients today!</p>
+    <p>Are you ready to get started? <a href="https://mangiare.vercel.app/">Log in</a> now and start cooking with your ingredients today!</p>
     <p>Best regards,</p>
     <p>The Mangiare Team</p>
     <p>P.S. Be sure to follow us on our <a href="#">social media</a> for cooking tips, recipes, and much more. We love sharing our creations with the Mangiare community!</p>
