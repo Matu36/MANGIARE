6d091e2cd6afb1a960b80ccf6daa564a728825da
Your Name~ <you@example.com>
2023-02-18 12:02:23 -0300

    shopping cart remove item alert

--- FRONT/src/pages/ShoppingCart/ShoppingCart.jsx
+++ FRONT/src/pages/ShoppingCart/ShoppingCart.jsx
@@ -5,140 +5,270 @@ import { useDispatch, useSelector } from "react-redux";
 import { LoginButton } from "../../components/Auth0/login_button";
 import { useAuth0 } from "@auth0/auth0-react";
 import background from "../../img/CartShopBackground.png";
-import {Box, HStack, Grid, Text, Button, IconButton, Image, Flex, Spacer, Input, InputGroup, InputRightElement, InputLeftElement, InputLeftAddon, InputRightAddon, Stack, Center, Divider, useToast, VStack} from "@chakra-ui/react";
+import {
+  Box,
+  HStack,  
+  Text,
+  Button,
+  Flex,
+  Image,
+  Spacer, 
+  VStack,
+  useToast, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay 
+} from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import logo from "../../img/LOGO 2.png";
 
-export default function ShoppingCart () {
-    const [orderState, setOrder] = React.useState();
-    const cart = useSelector((state) => state.cart.cart);
-    const dispatch = useDispatch();
-    const {email} = useAuth0().user || {email: null};
-    const { user, isAuthenticated } = useAuth0();
+export default function ShoppingCart() {
+  const [orderState, setOrder] = React.useState();
+  const cart = useSelector((state) => state.cart.cart);
+  const dispatch = useDispatch();
+  const { email } = useAuth0().user || { email: null };
+  const { user, isAuthenticated } = useAuth0();
 
-    //                   --------------- localStorage ---------------
-    useEffect(() => {
-        let LS_cart = JSON.parse(localStorage.getItem("MANGIARE_cart"));
-        if (!LS_cart) return;
-        else {
-        dispatch(setCart(LS_cart));
-        if (isAuthenticated) {
-            localStorage.setItem("MANGIARE_user", JSON.stringify(user.email));
-            localStorage.setItem("MANGIARE_userInfo", JSON.stringify(user))
-        }
-        }
-    }, [user, isAuthenticated]);
+                // =============  ALERTA PARA EL USUARIO AL REMOVER ITEM  =============
+                const [isAlertOpen, setIsAlertOpen] = React.useState(false);
+                const [idToDelete, setIdToDelete] = React.useState(null);
+                const [unitToDelete, setUnitToDelete] = React.useState(null);
+              
+                const cancelRef = React.useRef();
+              
+                const handleOnDelete = (id, unit) => {
+                  setIdToDelete(id);
+                  setUnitToDelete(unit);
+                  setIsAlertOpen(true);
+                };
+              
+                const handleDeleteConfirmation = () => {
+                  handleLocalStorage(idToDelete);
+                  dispatch(removeToCart({ id: idToDelete, unit: unitToDelete }));
+                  setIsAlertOpen(false);
+                  const toast = useToast();
+                  toast({
+                    title: "Item removed",
+                    description: "The item has been successfully removed from your cart.",
+                    status: "success",
+                    duration: 3000,
+                    isClosable: true,
+                  });
+                };
+              
+                const handleDeleteCancel = () => {
+                  setIsAlertOpen(false);
+                };
 
-    const handleLocalStorage = (id) => {
-        let LS_cart = JSON.parse(localStorage.getItem("MANGIARE_cart"));
-        localStorage.setItem("MANGIARE_cart", JSON.stringify(LS_cart.filter(i => i.id !== id)));
-      };
-    //                 --------------- fin localStorage ---------------
-
-    const handleOnDelete = (id, unit) => {
-        handleLocalStorage(id)
-        dispatch(removeToCart({id, unit}));
-    };
-
-    const handleOnChange = ({target}, unit) => {
-        dispatch(setCart(cart.map(el => ((el.id == target.id) && (el.unit == unit)) ? {...el, amount: target.value} : el)));
-    };
-
-    const handleCheckout = () => {
-        fetch(`http://localhost:3001/checkout`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({user: email, cart}),
-          })
-            .then(data => data.json())
-            .then(order => {
-                dispatch(setCart([]));
-                localStorage.removeItem('cart');
-                setOrder(order)
-                localStorage.setItem('pendingPayment', JSON.stringify({user: email, order}))
-            })
+  //                   --------------- localStorage ---------------
+  useEffect(() => {
+    let LS_cart = JSON.parse(localStorage.getItem("MANGIARE_cart"));
+    if (!LS_cart) return;
+    else {
+      dispatch(setCart(LS_cart));
+      if (isAuthenticated) {
+        localStorage.setItem("MANGIARE_user", JSON.stringify(user.email));
+        localStorage.setItem("MANGIARE_userInfo", JSON.stringify(user));
+      }
     }
+  }, [user, isAuthenticated]);
 
-    const handlePay = () => {
-        const {order, user} = JSON.parse(localStorage.getItem('pendingPayment'));
-        const {orderDetail, orderId} = order;
+  const handleLocalStorage = (id) => {
+    let LS_cart = JSON.parse(localStorage.getItem("MANGIARE_cart"));
+    localStorage.setItem(
+      "MANGIARE_cart",
+      JSON.stringify(LS_cart.filter((i) => i.id !== id))
+    );
+  };
+  //                 --------------- fin localStorage ---------------
 
-        fetch(`http://localhost:3001/payment`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({orderDetail, user, orderId})
-          })
-            .then(data => data.json())
-            .then(({response}) => window.open(response.init_point))
-            .then(() => {
-                localStorage.removeItem('pendingPayment');
-                window.location.href = '/home';
-            })
-    }
+//   const handleOnDelete = (id, unit) => {
+//     handleLocalStorage(id);
+//     dispatch(removeToCart({ id, unit }));
+//   };
 
-    return (
-        < Box
-        width="100%"
-        height="100vh"
-        marginTop="1px"        
-        style={{
-          display: "flex",
-          alignItems: "left",
-          justifyContent: "left",
-          flexDirection: "row",
-          backgroundSize: "cover",
-          backgroundPosition: "center center",
-        }}
-      >
-        <Flex>
-            <Box w='40%' h='100vh' bgImage={background} style={{          
-          backgroundSize: "cover",
-          }} />
-            <Spacer width='400px'/>
-            <Box w = '600px'>
-                <Text fontSize="6xl" fontWeight="bold" color="yellow.500" style={{textAlign: "center"}}>
-                  Shopping Cart
-                </Text>
-                <VStack spacing={10} align="stretch" my={10}>
-            {
-                (!cart?.length)
-                    ? <Text fontSize="2xl">The Shopping Cart is empty...</Text>
-                    : (<>
-                        <IngredientsList
-                            items = {cart.map(el => ({...el, units: [el.unit]}))}
-                            onChange = {handleOnChange}
-                            itemButton = {{
-                                caption: 'Remove Item',
-                                action: handleOnDelete
-                            }}
-                        />
-                        <HStack justify="space-between" align="flex-end" mt={10}>
-                        <Text fontSize="3xl" fontWeight="bold" color="green.500" style={{textAlign: "left"}}>
-                            Total: ${cart.reduce((aux, el) => aux + el.amount * el.price, 0).toFixed(2)}</Text>
-                        <center>
-                        {
-                            email
-                                ? <Button colorScheme="teal"  variant="solid" size="lg" onClick={handleCheckout}>
-                                Checkout
-                              </Button>
-                                : <><Text fontSize="lg" mr={2}>You must login before proceed to checkout</Text> <LoginButton /></>
-                        }
-                        </center>
-                        </HStack>
-                    </>)
-            }
-            {
-                JSON.parse(localStorage.getItem('pendingPayment')) && (<HStack justify="space-between" align="flex-end">
-                <Text fontSize="3xl" fontWeight="bold" color="green.500">Order #{JSON.parse(localStorage.getItem('pendingPayment')).order.orderId} has been created!</Text> <Button onClick={handlePay} colorScheme="green" size="lg">
-PAY
-</Button>
-</HStack>)
-            }
+  const handleOnChange = ({ target }, unit) => {
+    dispatch(
+      setCart(
+        cart.map((el) =>
+          el.id == target.id && el.unit == unit
+            ? { ...el, amount: target.value }
+            : el
+        )
+      )
+    );
+  };
 
-            </VStack>
+  const handleCheckout = () => {
+    fetch(`http://localhost:3001/checkout`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ user: email, cart }),
+    })
+      .then((data) => data.json())
+      .then((order) => {
+        dispatch(setCart([]));
+        localStorage.removeItem("cart");
+        setOrder(order);
+        localStorage.setItem(
+          "pendingPayment",
+          JSON.stringify({ user: email, order })
+        );
+      });
+  };
 
-                </Box>
-                <Box w = '600px'/>
-            </Flex>
+  const handlePay = () => {
+    const { order, user } = JSON.parse(localStorage.getItem("pendingPayment"));
+    const { orderDetail, orderId } = order;
+
+    fetch(`http://localhost:3001/payment`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ orderDetail, user, orderId }),
+    })
+      .then((data) => data.json())
+      .then(({ response }) => window.open(response.init_point))
+      .then(() => {
+        localStorage.removeItem("pendingPayment");
+        window.location.href = "/home";
+      });
+  };
+
+  return (
+    <Box
+      width="100%"
+      height="100vh"
+      marginTop="1px"
+      style={{
+        display: "flex",
+        alignItems: "left",
+        justifyContent: "left",
+        flexDirection: "row",
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+      }}
+    >
+      <Flex>
+        <Box
+          w="40%"
+          h="100vh"
+          bgImage={background}
+          style={{
+            backgroundSize: "cover",
+          }}
+        />
+        <Spacer width="400px" />
+        <Box w="600px">
+          <Text
+            fontSize="6xl"
+            fontWeight="bold"
+            color="yellow.500"
+            style={{ textAlign: "center" }}
+          >
+            Shopping Cart
+          </Text>
+          <VStack spacing={100} align="stretch" my={10}>
+            {!cart?.length ? (
+              <Text fontSize="2xl">The Shopping Cart is empty...</Text>
+            ) : (
+              <>
+                <IngredientsList
+                  items={cart.map((el) => ({ ...el, units: [el.unit] }))}
+                  onChange={handleOnChange}
+                  itemButton={{
+                    caption: "Remove Item",
+                    action: handleOnDelete,
+                  }}
+                />
+                <HStack justify="space-between" align="flex-end" mt={10}>
+                  <Text
+                    fontSize="3xl"
+                    fontWeight="bold"
+                    color="green.500"
+                    style={{ textAlign: "left" }}
+                  >
+                    Total: $
+                    {cart
+                      .reduce((aux, el) => aux + el.amount * el.price, 0)
+                      .toFixed(2)}
+                  </Text>
+                  <center>
+                    {email ? (
+                      <Button
+                        colorScheme="teal"
+                        variant="solid"
+                        size="lg"
+                        onClick={handleCheckout}
+                      >
+                        Checkout
+                      </Button>
+                    ) : (
+                      <>
+                        <Text fontSize="lg" mr={2}>
+                          You must login before proceed to checkout
+                        </Text>{" "}
+                        <LoginButton />
+                      </>
+                    )}
+                  </center>
+                </HStack>
+              </>
+            )}
+            {JSON.parse(localStorage.getItem("pendingPayment")) && (
+              <HStack justify="space-between" align="flex-end">
+                <Text fontSize="3xl" fontWeight="bold" color="green.500">
+                  Order #
+                  {
+                    JSON.parse(localStorage.getItem("pendingPayment")).order
+                      .orderId
+                  }{" "}
+                  has been created!
+                </Text>{" "}
+                <Button onClick={handlePay} colorScheme="green" size="lg">
+                  PAY
+                </Button>
+              </HStack>
+            )}
+            <NavLink to={"/home"} align='center'>
+              <Button colorScheme="teal" variant="solid" size="lg" >
+                Go Home
+              </Button>
+            </NavLink>
+          </VStack>
         </Box>
-    )
+        <AlertDialog
+        isOpen={isAlertOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={handleDeleteCancel}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+          
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Remove Item
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+                
+              Are you sure you want to remove this item from your cart?
+            </AlertDialogBody>
+            
+
+            <AlertDialogFooter>
+            <Flex align="center">
+    <Image src={logo} alt="logo" width="50px" height="50px" mr={4} />
     
+  </Flex>
+  <Spacer />
+              <Button ref={cancelRef} onClick={handleDeleteCancel}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" ml={3} onClick={handleDeleteConfirmation}>
+                Remove
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+        <Box w="600px" />
+      </Flex>
+    </Box>
+  );
 }
