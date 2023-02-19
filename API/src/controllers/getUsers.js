9336879d63c7d8f1fd0843b06fefc662d34f3047
YamilLeotta <yamil.leotta@live.com>
2023-02-19 06:16:42 -0300

    add getUsers controller

---
+++ API/src/controllers/getUsers.js
@@ -0,0 +1,24 @@
+const {Users} = require('../db.js');
+
+module.exports = async (req, res) => {
+  try{
+    if ((!req.body?.id) || (!req.body?.email) || (!req.body?.active)) throw 'No body params or inactive user'
+
+    let requestUser = await  Users.findOne({where: {id: req.body.id, email: req.body.email, active: true}});
+
+    if (!requestUser) return res.status(403).send('Wrong user');
+
+    let returnedUsers;
+
+    if (requestUser.dataValues.role !== null) returnedUsers = await Users.findAll();
+    else returnedUsers = await Users.findAll({where: {id: req.body.id}});
+
+    return (!returnedUsers)
+      ? res.status(404).send('Users Not Found')
+      : res.send(returnedUsers);
+  }
+  catch(error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
