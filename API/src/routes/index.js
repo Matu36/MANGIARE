ad9a065f9aa2aafbea776fcddb04870cc9485bda
YamilLeotta <yamil.leotta@live.com>
2023-02-19 06:27:39 -0300

    refact index Router

--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -5,15 +5,15 @@ const ingredientsRouter = require ('./ingredients');
 const favoritesRouter = require ('./favorites');
 const reviewsRouter = require ('./reviews');
 const usersRouter = require ('./users');
-const checkoutRouter = require ('./checkout');
 const paymentRouter = require ('./payment');
+const ordersRouter = require ('./orders');
 
 router.use("/recipes", recipesRouter);
 router.use("/ingredients", ingredientsRouter);
 router.use("/favorites", favoritesRouter);
 router.use("/reviews", reviewsRouter);
 router.use("/users", usersRouter);
-router.use("/checkout", checkoutRouter);
 router.use("/payment", paymentRouter);
+router.use("/orders", ordersRouter);
 
 module.exports = router;
