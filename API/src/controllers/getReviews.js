9d63e03064c1bff4bd8dd13470ba8665347b0ae6
YamilLeotta <yamil.leotta@live.com>
2023-02-19 16:47:41 -0300

    fix getReviews

--- API/src/controllers/getReviews.js
+++ API/src/controllers/getReviews.js
@@ -2,15 +2,14 @@ const {Reviews, Users} = require('../db.js');
 
 module.exports = async (req, res) => {
   try{
-    if ((!req.body?.id) || (!req.body?.email)) throw 'No body params or inactive user'
+    let requestUser;
 
-    let requestUser = await  Users.findOne({where: {id: req.body.id, email: req.body.email, active: true}});
-
-    if (!requestUser) return res.status(403).send('Wrong user');
+    if ((req.body?.id) && (req.body?.email))
+      requestUser = await  Users.findOne({where: {id: req.body.id, email: req.body.email, active: true}});
 
     let returnedReviews;
 
-    if (requestUser.dataValues.role !== null) returnedReviews = await Reviews.findAll();
+    if ((requestUser) && (requestUser?.dataValues.role !== null)) returnedReviews = await Reviews.findAll()
     else returnedReviews = await Reviews.findAll({where: {visible: true}});
 
     return (!returnedReviews)
