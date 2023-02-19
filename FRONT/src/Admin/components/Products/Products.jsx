a11e3d9bf27deebac976bf913058cd85af5bce52
Matu36 <matipineda85@live.com.ar>
2023-02-19 00:19:57 -0300

    Products

--- FRONT/src/Admin/components/Products/Products.jsx
+++ FRONT/src/Admin/components/Products/Products.jsx
@@ -3,7 +3,7 @@ import "./products.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getIngredients } from "../../../Redux/actions/ingredients";
 import Paginations from "../../../components/Paginations/Paginations";
-import { FcSearch } from "react-icons/fc";
+import { createIngredients } from "../../../Redux/actions/ingredients";
 
 import {
   Table,
@@ -32,7 +32,7 @@ export default function UserList() {
     };
   });
 
-                                  //Paginado
+  //PAGINADO
 
   const [currentPage, setCurrentPage] = useState(1); //Pagina Actual seteada en 1
   const [numberOfPage, setNumberOfPage] = useState(0); //Numero de Paginas seteado en 0
@@ -55,30 +55,68 @@ export default function UserList() {
     rows && setNumberOfPage(Math.ceil(rows.length / 9)); // cambiando el estado local de numeros de paginas a renderiza
   }, [rows, currentPage]);
 
-                                  //Fin paginado
+  //FIN PAGINADO
 
-                                   //  FILTER SEARCHBAR
-const [search, setSearch] = useState("");
+  //FILTER SEARCHBAR
+  const [search, setSearch] = useState("");
+  const [ingredients, setIngredients] = useState(rows);
 
-const handleOnChange = (e) => {
-  e.preventDefault();
-  setSearch(e.target.value)
-};
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
 
-const handleOnClick = (e) => {
-  e.preventDefault();
+  useEffect(() => {
+    !search ? setIngredients(rows) : filterByIngredients(search);
+  }, [ingredients, search]);
 
-  if (!search) {
-    alert("You dont write anything");
-  } else {
-    dispatch(getIngredients(search));
-    setSearch("");
-  }
-};
+  const filterByIngredients = (value) => {
+    let arrayCache = [...ingredients];
 
-  
+    arrayCache = arrayCache.filter((ingredient) =>
+      ingredient.name.toLowerCase().includes(value.toLowerCase())
+    );
 
-                               //FIN SEARCHBAR
+    setIngredients(arrayCache);
+  };
+
+  //FIN SEARCHBAR
+
+  //CREACION DE INGREDIENTE
+  const [create, setCreate] = useState({
+    name: "",
+    price: "",
+    units: "",
+    ingredients: [],
+  });
+  // para validar que no se repitan
+  const handleChangeCreate = (e) => {
+    setCreate({
+      ...create,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (
+      create.name &&
+      create.price &&
+      create.units &&
+      create.ingredients.length
+    ) {
+      dispatch(createIngredients(create)); // crea el ingrediente
+      alert("Ingredient has create");
+      setCreate({
+        name: "",
+        price: "",
+        units: "",
+        ingredients: [],
+      });
+    }
+  };
+
+  //FIN CREACION INGREDIENTE
 
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
@@ -96,18 +134,15 @@ const handleOnClick = (e) => {
     <div>
       <Table variant="simple" size="sm">
         <Thead>
-        <div>
-      <form onSubmit={handleOnClick}>
-        <FcSearch  onClick={handleOnClick} />
-        <input 
-          type="text"
-          placeholder="Search Ingredient "
-          onChange={handleOnChange}
-          value={search}
-          autoComplete="off"
-        />
-      </form>
-    </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search Ingredient "
+              onChange={handleOnChange}
+              value={search}
+              autoComplete="off"
+            />
+          </div>
           <Tr>
             {columns.map((column) => (
               <Th key={column.field}>{column.headerName}</Th>
@@ -115,7 +150,7 @@ const handleOnClick = (e) => {
           </Tr>
         </Thead>
         <Tbody>
-          {rows
+          {ingredients
             .slice(indexFirstPageRecipe(), indexLastPageRecipe())
             .map((row) => (
               <Tr key={row.id}>
@@ -124,6 +159,52 @@ const handleOnClick = (e) => {
                 ))}
               </Tr>
             ))}
+          <br />
+<div>
+          <form onSubmit={handleOnSubmit}>
+            <span>Create a Ingredient</span>
+            <label>
+              <p> Name</p>
+              <input
+                type="text"
+                name="name"
+                value={create.name}
+                autoComplete="off"
+                placeholder="Ingredient Name"
+                onChange={handleChangeCreate}
+              />
+            </label>
+
+            <label>
+              <p>Price</p>
+              <input
+                type="text"
+                name="price"
+                value={create.price}
+                autoComplete="off"
+                placeholder="Ingredient Price"
+                onChange={handleChangeCreate}
+              />
+            </label>
+
+            <label>
+              <p>Units</p>
+              <input
+                type="text"
+                name="units"
+                value={create.units}
+                autoComplete="off"
+                placeholder="Ingredient Units"
+                onChange={handleChangeCreate}
+              />
+            </label>
+            <div>
+              <button type="submit" tertiary>
+                Create Ingredient
+              </button>
+            </div>
+          </form>
+          </div>
           <div>
             {products && (
               <Paginations
--- FRONT/src/Redux/actions/ingredients.js
+++ FRONT/src/Redux/actions/ingredients.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 
 export const GET_INGREDIENTS = "GET_INGREDIENTS";
+export const CREATE_INGREDIENTS = "CREATE_INGREDIENTS";
 
 export const getIngredients = () => async (dispatch) => {
   let response = await axios.get(`/ingredients`);
   return dispatch({ type: GET_INGREDIENTS, payload: response.data });
   
+};
+
+export const createIngredients = (ingredients) => {
+  return async (dispatch) => {
+    try {
+      await axios.post(`/ingredients`, ingredients);
+      return dispatch({
+        type: CREATE_INGREDIENTS,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
