8cf65d4ab798e08a21294e1ecdda6db092537f23
YamilLeotta <yamil.leotta@live.com>
2023-02-19 06:26:05 -0300

    refact Users model

--- API/src/models/Users.js
+++ API/src/models/Users.js
@@ -2,18 +2,6 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Users', {
-    username: {
-      type: DataTypes.STRING(15),
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-        isAlpha: true,
-        isLowercase: true
-      },
-      set(value) {
-        this.setDataValue('username', value.toLowerCase());
-      }
-    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -22,24 +10,18 @@ module.exports = sequelize => {
         isEmail: true
       }
     },
-    password: {
-      type: DataTypes.STRING(15),
-      allowNull: false,
-      validate: {
-        notEmpty: true
-      },
+    address: {
+      type: DataTypes.STRING,
+      defaultValue: null
     },
     role: { // null: basic user ; false: admin user ; true: super admin user
       type: DataTypes.BOOLEAN,
       defaultValue: null
     },
-    active: { // null: not email verified ; false: banned/disabled ; true: active
+    active: { // false: banned/disabled ; true: active
       type: DataTypes.BOOLEAN,
-      defaultValue: null
-    },
-    premiumExpires: {
-      type: DataTypes.DATEONLY,
-      defaultValue: null
+      defaultValue: true,
+      allowNull: false
     }
   }, {
     updatedAt: false
