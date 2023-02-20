70a9d921bbb06b487482187c9dd86aa92021eaf8
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2023-02-19 22:47:35 -0300

    Post email from logged user into DB

--- API/src/app.js
+++ API/src/app.js
@@ -15,8 +15,8 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header("Access-Control-Allow-Origin", "https://mangiare.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  //res.header("Access-Control-Allow-Origin", "https://mangiare.vercel.app/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
