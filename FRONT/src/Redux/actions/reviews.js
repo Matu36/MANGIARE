b3cc908eb761f393da6d9dff0cad8c3d8a8a4cfd
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-21 13:26:17 -0300

    solved getReviews

--- FRONT/src/Redux/actions/reviews.js
+++ FRONT/src/Redux/actions/reviews.js
@@ -3,13 +3,16 @@ import axios from "axios";
 export const GET_REVIEWS = "GET_REVIEWS";
 
 export const getReviews = (currentUser) => {
-  let user = {
-    id: currentUser.id,
-    email: currentUser.email,
-  };
+  let user;
+  if (currentUser) {
+    user = {
+      id: currentUser.id,
+      email: currentUser.email,
+    };
+  }
   return async (dispatch) => {
     await axios
-      .get(`/reviews`, { params: user })
+      .get(`/reviews`, user ? { params: user } : null)
       .then((response) => response.data)
       .then((data) => {
         dispatch({ type: GET_REVIEWS, payload: data });
