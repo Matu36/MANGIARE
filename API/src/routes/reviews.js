d6cd5dfb31ecc060b185a503f1442c3dc2a80f5d
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2023-02-22 18:19:58 -0300

    Feat-API Function to ban reviews from users

--- API/src/routes/reviews.js
+++ API/src/routes/reviews.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const getReviews = require('../controllers/getReviews');
-const postReview = require('../controllers/postReview');
+const router = require("express").Router();
+const getReviews = require("../controllers/getReviews");
+const postReview = require("../controllers/postReview");
+const putReview = require("../controllers/putReview");
 
-router.get('/', getReviews);
-router.post('/', postReview);
+router.get("/", getReviews);
+router.post("/", postReview);
+router.put("/", putReview);
 
 module.exports = router;
