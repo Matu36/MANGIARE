ff8afa42be824fb7c60b959606a9e6756cdd88c2
YamilLeotta <yamil.leotta@live.com>
2023-02-21 21:38:38 -0300

    shop & payment flow

--- API/src/controllers/postOrder.js
+++ API/src/controllers/postOrder.js
@@ -1,21 +1,69 @@
 const {Users, Order_details, Orders} = require('../db.js');
+const mercadopago = require("mercadopago");
+const { merchant_orders } = require('mercadopago');
+const {MERCADOPAGO_KEY} = process.env;
+
+mercadopago.configure({ access_token: MERCADOPAGO_KEY });
 
 module.exports = async (req, res) => {
   try{
-    if ((!req.body?.cart) || (!req.body?.email) || (!req.body?.address)) throw 'No body params'; // || (!req.body?.userId)
+    if ((!req.body?.cart) || (!req.body?.email) || (!req.body?.userId) || (!req.body?.address)) throw 'No body params';
 
-    const userInstance = await Users.findOne({where: {email: req.body.email}}); // , id: req.body.userId
+    const userInstance = await Users.findOne({where: {email: req.body.email, id: req.body.userId}});
 
     if (!userInstance) throw 'Unregistred user';
 
-    const orderInstance = await Orders.create({userId: userInstance.dataValues.id});
+    let preference = {
+      items: req.body.cart.map(({amount, price}) => ({quantity: amount, unit_price: price})),
+      back_urls: {
+        success: "localhost:3000/orders",
+        failure: "localhost:3000/orders"
+      },
+      auto_return: "approved",
+      binary_mode: true,
+    };
+  
+    mercadopago.preferences
+      .create(preference)
+      .then(async (respPref) => {
+        //console.log('30', respPref.response);
 
-    await Order_details.bulkCreate(req.body.cart.map(({id, unit, amount, price}) => ({orderId: orderInstance.dataValues.id, ingredientId: id, unit, amount, price})))
-    
-    res.send({user: userInstance.dataValues, orderId: orderInstance.dataValues.id, orderDetail: req.body.cart.map(({id, name, amount, unit, price}) => ({title: name, description: amount + ' ' + unit, quantity: 1, currency_id: "$", unit_price: price * amount}))});
+        mercadopago.merchant_orders
+          .create({preference_id: respPref.response.id})
+          .then(async (respMerc) => {
+
+            //console.log('36', respMerc.response);
+
+            const orderInstance = await Orders.create({userId: userInstance.dataValues.id, preferenceId: respMerc.response.preference_id, merchant_orderId: respMerc.response.id});
+            // init_point: https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=48965690-ddb3e9dd-7963-474a-adc8-246bde9b0935
+
+            await Order_details.bulkCreate(req.body.cart.map(({id, unit, amount, price}) => ({orderId: orderInstance.dataValues.id, ingredientId: id, unit, amount, price})))
+      
+            res.send(orderInstance);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(400).send(error);
+      })
   }
   catch(error) {
     console.log(error);
-    res.status(500).send(error);
+    res.status(400).send(error);
   }
 };
+
+
+/* Para probar por postman
+
+{"userId": 3,
+"email": "email3@email.com",
+"address": "direccion de entrega 123",
+"cart": [
+    { "id": 4, "amount": 1, "unit": "pounds", "price": 10 },
+    { "id": 5, "amount": 2, "unit": "cup", "price": 20 },
+    { "id": 6, "amount": 3, "unit": "tablespoon", "price": 30 }
+    ]
+}
+
+*/
--- API/src/controllers/postPayment.js
+++ API/src/controllers/postPayment.js
@@ -1,27 +1,27 @@
-const mercadopago = require("mercadopago");
-const {MERCADOPAGO_KEY} = process.env;
+const {Orders} = require('../db.js');
 
-mercadopago.configure({ access_token: MERCADOPAGO_KEY });
+module.exports = async (req, res) => {
+  try{
+    if (req.body?.status === 'approved') await (await Orders.findOne({where: {status: 0, preferenceId: req.body?.preference_id}})).update({status: 1, tsPayment: new Date()});
 
-module.exports = (req, res) => {
-  let preference = {
-    items: req.body.orderDetail,
-    back_urls: {
-      success: "https://localhost:3000/home",
-      failure: "",
-      pending: "",
-    },
-    auto_return: "approved",
-    binary_mode: true,
-  };
+    res.send();
+  }
+  catch(error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 
-  mercadopago.preferences
-    .create(preference)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      console.log(error.message)
-      res.status(400).send({ error: error.message });
-    });
+  /*
+  if ((!req.body?.userId) || (!req.body?.email) || (!req.body?.orderId)) throw 'No body params'
+
+  let requestUser = await  Users.findOne({where: {id: req.body.userId, email: req.body.email}});
+  if (!requestUser) return res.status(403).send('Wrong user');
+
+  let returnedOrders = await requestUser.getOrders({where: {id: req.body.orderId, status: 0}, include: 'Order_details'});
+  if (!returnedOrders.length) return res.status(404).send('Order not found');
+
+  //return res.send(returnedOrders[0].Order_details.map(({amount, price}) => ({quantity: amount, unit_price: price})));
+  */
+
+
 };
