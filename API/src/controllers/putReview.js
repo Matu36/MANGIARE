73cb88dd2c65d37ae35c49b330a094183a4f6890
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2023-02-22 21:51:42 -0300

    Refact putReview function to handle as a Toggle to switch visible from true to false

--- API/src/controllers/putReview.js
+++ API/src/controllers/putReview.js
@@ -4,19 +4,25 @@ const putReview = async (req, res) => {
   try {
     let { recipeId, userId } = req.body;
     if (!recipeId && !userId) {
-      return res.status(404).send("No body Params");
+      return res.status(400).send("No body Params");
     }
-    let review = await Reviews.findOne({
-      where: { recipeId, userId, visible: true },
+    //como reviews no tiene una PK incremental no puedo seleccionar un review correcto a banear y por eso
+    //selecciono todos los comentarios de un usuario en una receta.
+    let reviews = await Reviews.findAll({
+      where: { recipeId, userId },
     });
-    if (!review) {
-      return res.status(404).send("Visible review not found");
+
+    if (!reviews || reviews.length === 0) {
+      return res.status(400).send("Visible reviews not found");
     }
-    let result = await review.update({ visible: false });
-    return res.status(200).send(result);
+    for (let review of reviews) {
+      await review.update({ visible: !review.visible });
+    }
+    return res.status(200).send("Review updated successfully");
   } catch (err) {
     console.log(err);
-    res.status(400).send(err.message);
+    res.status(500).send(err.message);
   }
 };
+
 module.exports = putReview;
