9929df02aac9843f7849dfe21fb057bf858c7c5c
YamilLeotta <yamil.leotta@live.com>
2023-02-24 16:17:23 -0300

    improve orders in user Panel

--- FRONT/src/pages/Orders/Orders.js
+++ FRONT/src/pages/Orders/Orders.js
@@ -7,15 +7,6 @@ export default function Orders(props) {
     const params = new URLSearchParams(window.location.search);
     const user = JSON.parse(localStorage.getItem("MANGIARE_user"));
     
-    if ((params.get('status') === 'approved') && (params.get('preference_id')))
-        fetch(`http://localhost:3001/payment`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({status: params.get('status'), preference_id: params.get('preference_id')}),
-            })
-                .then(data => data.json())
-//                .then(order => window.location.href = `/orders?id=${order.id}`)
-  
 /*
     useEffect(() => {
         console.log(state.orders);
@@ -51,20 +42,32 @@ export default function Orders(props) {
                 <tr>
                     {props.all ? <th>User Email</th> : ''}
                     <th>Order#</th>
-                    <th>createdAt#</th>
+                    <th>createdAt</th>
                     <th>Ingredients#</th>
-                    <th>Actions#</th>
+                    <th>Status</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
             {
                 state.orders?.map((el, idx) => (
                     <React.Fragment key={idx}>
-                        <tr className={((el.id === state.orderActive) || (!state.orderActive && el.id == params.get('id'))) ? s.orderItem : ((idx % 2) ? '' : s.par)} onClick={() => setState({...state, orderActive: el.id})}>
+                        <tr className={((el.id === state.orderActive) || (!state.orderActive && el.id === props.order_id)) ? s.orderItem : ((idx % 2) ? '' : s.par)} onClick={() => setState({...state, orderActive: el.id})}>
                             {props.all ? <td>{el.User.email}</td> : ''}
                             <td>{el.id}</td>
                             <td>{el.createdAt}</td>
                             <td>{el.Order_details.length}</td>
+                            <td>{
+                                (el.status === 0)
+                                    ? 'Payment pending'
+                                    : (el.status === 1)
+                                        ? 'Stock pending'
+                                        : (el.status === 2)
+                                            ? 'In preparation'
+                                            : (el.status === 3)
+                                                ? 'Sent'
+                                                : 'Canceled'
+                                }</td>
                             <td>
                                 {(((!el.status) || (el.status === 1) && (user.role !== null) && props.all)) ? <button onClick={() => handleStatus(4, el.id)}>Cancel</button> : ''}
 
@@ -73,8 +76,8 @@ export default function Orders(props) {
                                 {((el.status === 2) && (user.role !== null) && (props.all)) ? <button onClick={() => handleStatus(3, el.id)}>Send</button> : ''}
                             </td>
                         </tr>
-                        {((state.orderActive === el.id) || (!state.orderActive && el.id == params.get('id'))) ? <tr className={s.orderItem}>
-                            <td colSpan={props.all ? 5 : 4} style={{padding: '30px 0'}}>
+                        {((state.orderActive === el.id) || (!state.orderActive && el.id === props.order_id)) ? <tr className={s.orderItem}>
+                            <td colSpan={props.all ? 6 : 5} style={{padding: '30px 0'}}>
                                 <IngredientsList items={el.Order_details.map(({IngredientId, amount, unit, price, Ingredient}) => ({id: IngredientId, amount, unit, price, name: Ingredient.name}))} orderDetail={true}/>
                             </td>
                         </tr> : ''}
