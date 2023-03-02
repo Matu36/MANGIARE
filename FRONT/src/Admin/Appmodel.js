f2d1165d811149fd6f5e508b988de71e465cef77
gernavarrete <germannavarrete.info@gmail.com>
2023-03-02 19:53:12 -0300

    Page Not Found

--- FRONT/src/Admin/Appmodel.js
+++ FRONT/src/Admin/Appmodel.js
@@ -4,16 +4,29 @@ import SideBar from "./components/sideBar/SideBar";
 import TopBar from "./components/topBar/TopBar";
 import HomeAdmin from "./pages/home/HomeAdmin";
 import NavBar from "./../components/NavBar/NavBar.jsx";
+import { useAuth0 } from "@auth0/auth0-react";
+import NotFound from "../pages/NotFound/NotFound";
 
 export default function Appmodel() {
+  const { isAutenticated } = useAuth0();
+  const currentUser = JSON.parse(localStorage.getItem("MANGIARE_user"));
+  console.log(isAutenticated, currentUser);
+
   return (
     <div className="contain">
-      <NavBar />
-      <TopBar />
-      <div className="divContainerAdmin">
-        <SideBar />
-        <HomeAdmin />
-      </div>
+      {(!isAutenticated && currentUser?.role === undefined) ||
+      currentUser?.role === null ? (
+        <NotFound />
+      ) : (
+        <div>
+          <NavBar />
+          <TopBar />
+          <div className="divContainerAdmin">
+            <SideBar />
+            <HomeAdmin />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
